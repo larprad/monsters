c1cee7208c860513ce4d0d612357874d50122702
larprad <59915248+larprad@users.noreply.github.com>
2020-06-11 17:44:44 +0200

    make home layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style/App.css';
+import monsters from './data/monsters';
+// import monsters from './data/monsters';
+
+function Header() {
+  return (
+    <header className="App-header flexCenter">
+      <h1>Mage Knight Monsters</h1>
+    </header>
+  );
+}
+
+function MonsterStore() {
+  return (
+    <div className="App-monsters">
+      <MonsterTuile />
+      <MonsterTuile />
+      <MonsterTuile />
+      <MonsterTuile />
+      <MonsterTuile />
+      <MonsterTuile />
+      <MonsterTuile />
+      <MonsterTuile />
+      <MonsterTuile />
+      <MonsterTuile />
+      <MonsterTuile />
+      <MonsterTuile />
+    </div>
+  );
+}
+
+function MonsterTuile() {
+  const { img, name, description } = monsters.skeleton;
+  return (
+    <div className="monsterTuile flexCenter spaceStart">
+      <img src={img} alt={name} />
+      <div>
+        <h3>{name}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+function Actions() {
+  return (
+    <div className="App-actions flexCenter spaceEvenly">
+      <button>Filter</button>
+      <button>New</button>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <MonsterStore />
+      <Actions />
     </div>
   );
 }
---
+++ src/data/monsters.js
@@ -0,0 +1,23 @@
+export default {
+  skeleton: {
+    size: 'medium',
+    name: 'Skeleton',
+    description: 'Undead willing to fight',
+    img:
+      'https://images.pexels.com/photos/1528375/pexels-photo-1528375.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  },
+  troll: {
+    size: 'big',
+    name: 'Troll',
+    description: 'Big smash with wood stick',
+    img:
+      'https://images.pexels.com/photos/4075/nature-walking-animal-strong.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  },
+  dragon: {
+    size: 'big',
+    name: 'Dragon',
+    description: 'King of the sky',
+    img:
+      'https://images.pexels.com/photos/3963663/pexels-photo-3963663.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  },
+};
